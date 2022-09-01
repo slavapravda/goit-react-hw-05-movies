73d0ac5fb7f04ad7afaab1c9081cd8452b5e0b47
Slava Pravda <pravdaslava7@gmail.com>
2022-09-01 14:20:49 +0300

    change for hw 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,7 @@
-import { useState } from 'react';
-import Searchbar from './Searchbar/Searchbar';
-import ImageGallery from './ImageGallery/ImageGallery';
-
 export const App = () => {
-  const [searchName, setName] = useState('');
-  const [page, setPage] = useState(1);
-
-  const formSearchHandler = ({ searchName }) => {
-    setName(searchName);
-    setPage(1);
-  };
-
-  const onLoadMoreBtnClick = () => setPage(prevPage => prevPage + 1);
-
   return (
     <div>
-      <Searchbar onSubmit={formSearchHandler} searchName={searchName} />
-      <ImageGallery
-        searchName={searchName}
-        page={page}
-        onLoadMoreBtnClick={onLoadMoreBtnClick}
-      />
+      <p>привет</p>
     </div>
   );
 };
