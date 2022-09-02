e06e9df257ebe6bd864004d7eccd69729231424d
Slava Pravda <pravdaslava7@gmail.com>
2022-09-02 10:30:14 +0300

    add

--- src/pages/SingleMoviePage/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage/SingleMoviePage.jsx
@@ -23,8 +23,7 @@ const SingleMoviePage = () => {
 
         const movie = await singleMovie(id);
         setMovie(movie);
-        console.log(movie)
-
+        console.log(movie);
       } catch (error) {
         setError(error);
       } finally {
@@ -43,27 +42,29 @@ const SingleMoviePage = () => {
       </button>
       {loading && <p>Загрузка...</p>}
       {error && <p>{error.message}</p>}
-      <div className={s.pagePosition}>
-        <img
-          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-          alt={movie.title}
-          width={420}
-        />
-        <div className={s.pageRight}>
-          <div className={s.pageRightShadow}>
-            <h2>{movie.title}</h2>
-            <p>User score: {Math.round(movie.vote_average)} %</p>
-          </div>
-          <div className={s.pageRightShadow}>
-            <h3>Overview</h3>
-            <p>{movie.overview}</p>
-          </div>
-          <div className={s.pageRightShadow}>
-            <h4>Genres</h4>
-            <p>{movieGenres}</p>
+      {movie && (
+        <div className={s.pagePosition}>
+          <img
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            alt={movie.title}
+            width={420}
+          />
+          <div className={s.pageRight}>
+            <div className={s.pageRightShadow}>
+              <h2>{movie.title}</h2>
+              <p>User score: {Math.round(movie.vote_average)} %</p>
+            </div>
+            <div className={s.pageRightShadow}>
+              <h3>Overview</h3>
+              <p>{movie.overview}</p>
+            </div>
+            <div className={s.pageRightShadow}>
+              <h4>Genres</h4>
+              <p>{movieGenres}</p>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
